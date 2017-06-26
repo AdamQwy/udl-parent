21206bd53c0669cb2bbf1df9c50d9f408d085270
AdamQwy <283470798@qq.com>
2017-06-26 18:17:13 +0800

    move pojo from dao to pojo. create interface of user.

---
+++ udl-interface/src/main/java/com/qwy/service/UserService.java
@@ -0,0 +1,21 @@
+package com.qwy.service;
+
+import com.qwy.pojo.User;
+
+/**
+ * <p>
+ * Title: UserService
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Company: CodeChewer
+ * </p>
+ * 
+ * @author AdamQwy
+ * @date 2017年6月26日 下午6:13:16
+ */
+public interface UserService {
+	public void registe(User user);
+}
