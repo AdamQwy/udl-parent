b4c7bf6399628fa6cedb74c9a267f56c500eb057
AdamQwy <283470798@qq.com>
2017-06-26 16:38:14 +0800

    init and ignore

---
+++ udl-web/src/test/java/com/qwy/controller/demo.java
@@ -0,0 +1,24 @@
+package com.qwy.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class demo {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
